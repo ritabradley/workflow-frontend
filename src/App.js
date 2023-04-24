29d63a18770686d9f870aa81c78988b1945c79e5
Restyled.io <commits@restyled.io>
2023-04-24 21:25:25 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name = 'Rita Bradley';
-    return (
-        <div className='App'>
-            <header className='App-header'>
-                <h1>Hi, I'm {name}!</h1>
-                <p>I'm just your friendly, neighborhood web developer. Connect with me on Twitter</p>
-                <a
-    className = 'App-link'
-    href = 'https://twitter.com/ritabradley_dev'
-    target = '_blank'
-    rel = 'noopener noreferrer' > My Twitter</a>
-            </header><
-          /div>
-    );
+  const name = "Rita Bradley";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, I'm {name}!</h1>
+        <p>
+          I'm just your friendly, neighborhood web developer. Connect with me on
+          Twitter
+        </p>
+        <a
+          className="App-link"
+          href="https://twitter.com/ritabradley_dev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          My Twitter
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import {render, screen} from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 
-import App from './App';
+import App from "./App";
 
-test('renders Rita Bradley as name', () => {
+test("renders Rita Bradley as name", () => {
   render(<App />);
   const name = screen.getByText(/Rita Bradley/i);
   expect(name).toBeInTheDocument();
 });
 
-test('renders a link to my Twitter', () => {
+test("renders a link to my Twitter", () => {
   render(<App />);
   const linkElement = screen.getByText(/My Twitter/i);
   expect(linkElement).toBeInTheDocument();

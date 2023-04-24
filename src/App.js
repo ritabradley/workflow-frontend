400d8be74058102da5cf9fc2cda4c33b4d019d4c
Restyled.io <commits@restyled.io>
2023-04-24 21:25:23 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
 
 function App() {
-    const name = 'Rita Bradley';
+  const name = 'Rita Bradley';
     return (
         <div className='App'>
             <header className='App-header'>
                 <h1>Hi, I'm {name}!</h1>
                 <p>I'm just your friendly, neighborhood web developer. Connect with me on Twitter</p>
                 <a
-                    className='App-link'
-                    href='https://twitter.com/ritabradley_dev'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    My Twitter
-                </a>
-            </header>
-        </div>
+    className = 'App-link'
+    href = 'https://twitter.com/ritabradley_dev'
+    target = '_blank'
+    rel = 'noopener noreferrer' > My Twitter</a>
+            </header><
+          /div>
     );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+
 import App from './App';
 
 test('renders Rita Bradley as name', () => {
-    render(<App />);
-    const name = screen.getByText(/Rita Bradley/i);
-    expect(name).toBeInTheDocument();
+  render(<App />);
+  const name = screen.getByText(/Rita Bradley/i);
+  expect(name).toBeInTheDocument();
 });
 
 test('renders a link to my Twitter', () => {
-    render(<App />);
-    const linkElement = screen.getByText(/My Twitter/i);
-    expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  const linkElement = screen.getByText(/My Twitter/i);
+  expect(linkElement).toBeInTheDocument();
 });
